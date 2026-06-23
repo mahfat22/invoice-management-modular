92752eb6e654f5bd58199e21016a258f8849626d
mahfat22 <mahmoud.ragaey.99@gmail.com>
2026-06-23 12:02:57 +0300

    Setup modular architecture and create base modules

---
+++ Modules/Invoices/database/migrations/2026_06_23_090218_create_invoice_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained('invoices')->onDelete('cascade');
+
+            $table->string('item_name');
+            $table->integer('quantity')->default(1); 
+            $table->decimal('unit_price', 10, 2)->default(0.00);
+            $table->decimal('discount_amount', 10, 2)->default(0.00);
+            $table->decimal('tax_amount', 10, 2)->default(0.00);
+            $table->decimal('line_total', 10, 2)->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_items');
+    }
+};
